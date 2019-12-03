504fdf23d5f98f105553e2c9f7f1c6249a79b1af
LuizMarcello <luizmarcello.vmo@hotmail.com>
2019-12-03 07:17:56 -0300

    Seeding Service

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 using Pomelo.EntityFrameworkCore.MySql;
+using SalesWebMvc.Data;
 
 namespace SalesWebMvc
 {
@@ -40,14 +41,18 @@ namespace SalesWebMvc
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
                          builder.MigrationsAssembly("SalesWebMvc")));
+
+            //Registrando esta classe/serviço no sistema de injeção de dependência da aplicação.
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
